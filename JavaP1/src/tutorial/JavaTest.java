841ba7d5b301591fbaedd4f5422b9adb94764e64
Snow <andy0509rey@gmail.com>
2024-01-09 12:12:01 +0100

    First few lines of Code

--- JavaP1/src/tutorial/JavaTest.java
+++ JavaP1/src/tutorial/JavaTest.java
@@ -2,6 +2,10 @@ package tutorial;
 
 public class JavaTest {
 	  public static void main(String[] args) {
+		  
 	    System.out.println("Hello World");
+	    String nombre = "Andy";
+	    System.out.println("Hola" + nombre);
+	    
 	  }
 	}
