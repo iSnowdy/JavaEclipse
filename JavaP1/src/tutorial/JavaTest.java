c5ee3fdbb8f06e441c9420671b0e19cd49b5a88c
Snow <andy0509rey@gmail.com>
2024-01-09 13:10:43 +0100

    Commit&Push

--- JavaP1/src/tutorial/JavaTest.java
+++ JavaP1/src/tutorial/JavaTest.java
@@ -9,7 +9,8 @@ public class JavaTest {
 	    
 	    String centro_educativo = "Cesur";
 	    System.out.println(nombre + "studies Web Development in " + centro_educativo);
-	    String not_good = "aaa";
+	    
+	    String last_name = "López"; 
 	    
 	  }
 	}
